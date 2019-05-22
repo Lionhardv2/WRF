efa80ec71ab7b174abdb0bb1565ac6d39c074474
Opti3040A <opti3040a@hermes>
2019-05-22 14:19:13 -0400

    Corrigiendo el tipo de Dato y reemplazando los datos faltantes por el promedio

--- weibull1.py
+++ weibull1.py
@@ -25,3 +25,24 @@ for column in missing_data.columns.values.tolist():
     print(missing_data[column].value_counts())
     print(" ")
 print(df.dtypes)
+#******************************************************************
+#		Reemplazando los datos faltantes por el promedio 
+#******************************************************************
+column = "Viento - Velocidad (m/s)"
+print("Datos sin reemplazar")
+print(missing_data[column].value_counts())
+# Calculando el promedio de la columna
+avg = df[column].astype("float").mean(axis=0)
+# Reemplazando los datos nan por el promedio
+df[column].replace(np.nan,avg, inplace = True)
+print("Reemplazando los datos nulos")
+missing_data = df.isnull()
+print(missing_data[column].value_counts())
+#******************************************************************
+#			corrigiendo el tipo de dato
+#******************************************************************
+print(df[column].dtype)
+# cambiando el dato a tipo flotante
+df[column] = df[column].astype("float")
+print(df[column].dtype)
+print(df[column].head())
