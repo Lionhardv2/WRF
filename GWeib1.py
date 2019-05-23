73e4571406121da3daa40d87ffbf33868a9f305b
Opti3040A <opti3040a@hermes>
2019-05-23 08:48:33 -0400

    Archivo de generacion de graficas de distribucion Weibull

---
+++ GWeib1.py
@@ -0,0 +1,8 @@
+#******************************************************************
+#		Funcion de Ditribucion de Probabilidades de Weibull
+#******************************************************************
+
+import weibull
+import pandas as pd
+import numpy as np
+
