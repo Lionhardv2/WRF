de73945646c0aa4676cdea363662bfff104dec93
Opti3040A <opti3040a@hermes>
2019-05-31 14:03:37 -0400

    actua1

--- Filtrado10.R
+++ Filtrado10.R
@@ -4,5 +4,10 @@
 
 # Declarando las librerias a utilizar
 # -----------------------------------
+library(stats)
+library(dplyr)
 
-
+# Capturando los datos en bruto
+# -----------------------------
+Datos_CSV <- "/home/opti3040a/Dropbox/CMSN/01WRF_Project/01_QMet/20150919-20180828.csv"         # Datos Observados Qollpana
+df <- read.csv(file=Datos_CSV)
